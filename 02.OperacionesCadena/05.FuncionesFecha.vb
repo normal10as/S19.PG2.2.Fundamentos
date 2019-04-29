f011b30115e4aee8b0b6a04c72dd106b79b3d9cc
Fabio Dos Santos <fabio2st@yahoo.com>
2019-04-29 20:49:51 -0300

    Funciones de fecha

--- 02.OperacionesCadena/05.FuncionesFecha.vb
+++ 02.OperacionesCadena/05.FuncionesFecha.vb
@@ -4,13 +4,18 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Ahora: " & now)
         Dim TestDateTime As Date = #1/27/2019 5:04:23 PM#
-        Console.WriteLine(Format(TestDateTime, "Long Time"))
-        Console.WriteLine(Format(TestDateTime, "h:m:s"))
-        Console.WriteLine(Format(TestDateTime, "dddd, MMM d yyyy"))
-        Console.WriteLine(Format(TestDateTime, "Long Time"))
-        Console.WriteLine(Format(TestDateTime, "Long Time"))
-        Console.WriteLine(Format(TestDateTime, "Long Time"))
-        Console.WriteLine(Format(TestDateTime, "Long Time"))
+        Console.WriteLine("Fecha: " & TestDateTime)
+        TestDateTime = New DateTime(2019, 4, 20)
+        Console.WriteLine(TestDateTime)
+        TestDateTime = New DateTime(2019, 4, 20, 12, 35, 30)
+        Console.WriteLine(TestDateTime)
+        Console.WriteLine("Día de la semana: " & Now.DayOfWeek)
+        Console.WriteLine("Mes: " & Now.Month)
+        Console.WriteLine("Segundos: " & Now.Second)
+
+        Console.WriteLine("Fecha: " & TestDateTime.AddDays(10))
+        Console.WriteLine("Fecha: " & TestDateTime.AddMonths(-2))
+        Console.WriteLine("Fecha: " & TestDateTime.AddHours(5))
 
     End Sub
 End Module
