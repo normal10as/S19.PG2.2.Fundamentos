0165caf4630072121372e51d99bf541f34a2faac
Fabio Dos Santos <fabio2st@yahoo.com>
2019-06-03 18:23:43 -0300

    404

--- 09.EstructurasRepetitivas/DoWhile.vb
+++ 09.EstructurasRepetitivas/DoWhile.vb
@@ -1,9 +1,5 @@
 ﻿Module DoWhile
     Sub Hacer()
-        Console.WriteLine("Ingrese una palabra para verificar si es Palíndromo")
-        Dim palabra As String
-        palabra = Console.ReadLine()
-        Do While palabra
-        Loop
+
     End Sub
 End Module
