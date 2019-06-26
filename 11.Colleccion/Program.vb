4ef5d2c0dfa9b84f0e72d7bc03bd1e2590fe2618
Fabio Dos Santos <fabio2st@yahoo.com>
2019-06-26 18:14:59 -0300

    ok

---
+++ 11.Colleccion/Program.vb
@@ -0,0 +1,32 @@
+Imports System
+Imports System.Collections.ObjectModel
+
+Module Program
+    Sub main()
+        ' crea la coleccion
+        Dim frutas As Collection
+        ' inicializa la colección
+        frutas = New Collection
+        ' agregar elementos
+        frutas.Add("Manzanas", "mz")
+        frutas.Add("Peras", "pe")
+        frutas.Add("Frutillas", "fr")
+        frutas.Add("Tomate", "to")
+        Console.WriteLine("CAntidad: " & frutas.Count)
+        ' iterar
+        frutas.Remove(4)
+        For Each fruta In frutas
+            Console.WriteLine(fruta)
+        Next
+
+        Console.WriteLine("segundo: " & frutas.Item(2))
+        Console.WriteLine("segundo: " & frutas.Item("fr"))
+        frutas.Clear()
+        Console.WriteLine("borrado todo")
+        For Each fruta In frutas
+            Console.WriteLine(fruta)
+        Next
+
+
+    End Sub
+End Module
